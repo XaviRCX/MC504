e992ad3d0ae068c358dab9c96e2fedc7d8a79a41
MatBGuira <92648439+MatBGuira@users.noreply.github.com>
2023-05-14 04:16:24 -0300

    Update screen.py

--- screen.py
+++ screen.py
@@ -1,76 +1,15 @@
 import os, time
 import numpy as np
 
-tick = 1
-
 np.set_printoptions(threshold=np.inf)
 
-I = np.full((30, 100)," ")
-#I[0,:] = '#'
-#I[-1,:] = '#'
-#I[:,0] = '#'
-#I[:,-1] = '#'
-I[10,:] = "-"
-I[19,:] = "-"
-I[11:18,36] = "_"
-I[11:18,37] = "_"
-I[11:18,38] = "_"
-I[11:18,39] = "_"
-I[11:18,-6] = "_"
-I[11:18,-7] = "_"
-I[11:18,-8] = "_"
-I[11:18,-9] = "_"
-
-def printScreen():
-  for l in I:
-    print("".join(l))
-
-def refresh():
-  os.system('clear')
-  printScreen()
-
-def placeScreen(O, l, c):
-  I[l:O.shape[0]+l, c:O.shape[1]+c] = O
-
-def empty(n):
-  if n == 1:
-    placeScreen(np.full((4, 21)," "), 3, 3)
-  elif n == 2:
-    placeScreen(np.full((4, 21)," "), 24, 3)
-  elif n == 3:
-    placeScreen(np.full((4, 21)," "), 3, 58)
-  elif n == 4:
-    placeScreen(np.full((4, 21)," "), 24, 58)
-  refresh()
-
-def ride(extended):
-  time.sleep(tick)
-  placeScreen(car, 13, 3)
-  refresh()
-  time.sleep(tick)
-  placeScreen(np.full((4, 21)," "), 13, 3)
-  refresh()
-  if extended == 1:
-    placeScreen(car, 13, 58)
-    refresh()
-    time.sleep(tick)
-    placeScreen(np.full((4, 21)," "), 13, 58)
-    refresh()
-
-def park(n):
-  if n == 1:
-    ride(0)
-    placeScreen(car, 3, 3)
-  elif n == 2:
-    ride(0)
-    placeScreen(car, 24, 3)
-  elif n == 3:
-    ride(1)
-    placeScreen(car, 3, 58)
-  elif n == 4:
-    ride(1)
-    placeScreen(car, 24, 58)
-  refresh()
+S = np.full((29, 149)," ")
+S[10,:90] = "-"
+S[18,:90] = "-"
+S[11:17,33:37] = "_"
+S[11:17,78:82] = "_"
+S[:,90] = "|"
+S[:,148] = "|"
 
 #(9, 14)
 pump = np.array([
@@ -85,53 +24,126 @@ pump = np.array([
   ['[','_','_','_','_','_','_','_','_','_','_','_','_',']']
 ])
 
-#(4, 21)
+#(5, 21)
 car = np.array([
+[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
 [' ',' ',' ',' ',' ',' ','.','-','-','-','-','-','-','-','-','.',' ',' ',' ',' ',' '],
 [' ','_','_','_','_','/','_','_','_','_','_','|','_','_','_',' ','\\','_','_','_',' '],
 ['O',' ',' ',' ',' ','_',' ',' ',' ','-',' ','|',' ',' ',' ','_',' ',' ',' ',',','*'],
 [' ','\'','-','-','(','_',')','-','-','-','-','-','-','-','(','_',')','-','-','\'',' ']
 ])
 
-#(8, 27)
-parking = np.full((8, 27)," ")
-parking[0,:] = '#'
-parking[-1,:] = '#'
-parking[:,0] = '#'
-parking[:,-1] = '#'
+P = np.array([
+['#','#','#','#','#','#','#'],
+['#','#',' ',' ',' ','#','#'],
+['#','#','#','#','#','#','#'],
+['#','#',' ',' ',' ',' ',' '],
+['#','#',' ',' ',' ',' ',' ']
+])
 
+#(9, 27)
+parking_pump = np.full((9, 27),"#")
+parking_pump[1:-1,1:-1] = " "
 
+#(9, 27)
+parking = np.full((9, 27),"+")
+parking[1:-1,1:-1] = " "
 
-placeScreen(parking, 1, 0)
-placeScreen(parking, 22, 0)
-placeScreen(parking, 1, 55)
-placeScreen(parking, 22, 55)
+def printScreen():
+  for l in S:
+    print("".join(l))
 
-placeScreen(pump, 0, 31)
-placeScreen(pump, 21, 31)
-placeScreen(pump, 0, 86)
-placeScreen(pump, 21, 86)
+def placeScreen(O, y, x):
+  S[y:O.shape[0]+y, x:O.shape[1]+x] = O
 
-placeScreen(np.array([['1','0','0','%']]), 2, 12)
-placeScreen(np.array([['1','0','0','%']]), 23, 12)
-placeScreen(np.array([['1','0','0','%']]), 2, 67)
-placeScreen(np.array([['1','0','0','%']]), 23, 67)
+cfx1 = 3
+cfx2 = 48
+cpx1 = 95
+cpx2 = 123
+cy1 = 2
+cy2 = 22
+cry = 12
 
-park(1)
-park(2)
-park(3)
-park(4)
+def refresh():
+  os.system('clear')
+  printScreen()
+  S[10,:90] = "-"
+  S[18,:90] = "-"
+  S[11:17,33:37] = "_"
+  S[11:17,78:82] = "_"
+  S[:,90] = "|"
+  S[:,148] = "|"
+  placeScreen(P, 12, 116)
 
-ride(1)
-ride(1)
+placeScreen(parking_pump, 0, 0)
+placeScreen(parking_pump, 20, 0)
+placeScreen(parking_pump, 0, 45)
+placeScreen(parking_pump, 20, 45)
 
-empty(1)
-empty(2)
-empty(3)
-empty(4)
+placeScreen(pump, 0, 28)
+placeScreen(pump, 20, 28)
+placeScreen(pump, 0, 73)
+placeScreen(pump, 20, 73)
 
+placeScreen(parking, 0, 92)
+placeScreen(parking, 20, 92)
+placeScreen(parking, 0, 120)
+placeScreen(parking, 20, 120)
+placeScreen(P, 12, 116)
 
-#for i in range(1):
-#  printScreen(I)
-#  time.sleep(0.1)
-#  os.system('clear')
+def showLevel(tank):
+  C = car.copy()
+  level = str(tank) + '%'
+  if tank < 10:
+    level = '  ' + level
+  elif tank < 100:
+    level = ' ' + level
+  C[0, 9:13] = np.array(list(level)) 
+  return C
+
+def ride(x, tank):
+  placeScreen(showLevel(tank), cry, x)
+  refresh()
+
+def emptyRide(x):
+  placeScreen(np.full(car.shape," "), cry, x)
+  refresh()
+
+def park(n, tank):
+  if n == 1:
+    placeScreen(showLevel(tank), cy1, cfx1)
+  elif n == 2:
+    placeScreen(showLevel(tank), cy2, cfx1)
+  elif n == 3:
+    placeScreen(showLevel(tank), cy1, cfx2)
+  elif n == 4:
+    placeScreen(showLevel(tank), cy2, cfx2)
+  elif n == 5:
+    placeScreen(showLevel(tank), cy1, cpx1)
+  elif n == 6:
+    placeScreen(showLevel(tank), cy2, cpx1)
+  elif n == 7:
+    placeScreen(showLevel(tank), cy1, cpx2)
+  elif n == 8:
+    placeScreen(showLevel(tank), cy2, cpx2)
+  refresh()
+
+def empty(n):
+  if n == 1:
+    placeScreen(np.full(car.shape," "), cy2, cpx1)
+  elif n == 2:
+    placeScreen(np.full(car.shape," "), cy2, cpx1)
+  elif n == 3:
+    placeScreen(np.full(car.shape," "), cy2, cpx2)
+  elif n == 4:
+    placeScreen(np.full(car.shape," "), cy2, cpx2)
+  elif n == 5:
+    placeScreen(np.full(car.shape," "), cy2, cpx1)
+  elif n == 6:
+    placeScreen(np.full(car.shape," "), cy2, cpx1)
+  elif n == 7:
+    placeScreen(np.full(car.shape," "), cy2, cpx2)
+  elif n == 8:
+    placeScreen(np.full(car.shape," "), cy2, cpx2)
+  refresh()
+  
