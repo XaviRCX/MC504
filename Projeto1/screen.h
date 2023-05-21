957e42ab25304da7e0e4b9c8f2813dbb48772050
Lucas Xavier <80770939+XaviRCX@users.noreply.github.com>
2023-05-21 18:55:05 -0300

    Create screen.h

---
+++ Projeto1/screen.h
@@ -0,0 +1,18 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+ 
+void inicialize();
+  
+void end();
+
+void printScreen();
+  
+void ride(int x, int tank, bool reverse);
+  
+void emptyRide(int x);
+  
+void park(int n, int tank);
+  
+void empty(int n);
+
+#endif
