e98a1d8edbf84b79b24e989515d9996fe152df25
MatBGuira <92648439+MatBGuira@users.noreply.github.com>
2023-05-21 19:45:04 -0300

    Update screen.h

--- Projeto1/screen.h
+++ Projeto1/screen.h
@@ -5,14 +5,20 @@ void inicialize();
   
 void end();
 
-void printScreen();
-  
-void ride(int x, int tank, bool reverse);
-  
-void emptyRide(int x);
-  
+//n = 0 - 3, estacionamento
+//n = 4 - 7, posto
+//n = 8 passa direto pelas vagas
+
+//posiciona um carro em uma das 8 vagas;
 void park(int n, int tank);
-  
+ 
+//remove o carro de uma das 8 vagas
 void empty(int n);
+
+//faz a animacao de andar pela rua
+void riding(int n, int tank, int time);
+
+//anima o carro enchendo em uma das vagas
+void fill(int n, int tank, int time);
 
 #endif
